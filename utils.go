20f14ee2826935112cd7481ce1e2e1edd467983d
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-16 14:43:51 +0100

    Refactor

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package simpletimed
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -10,4 +11,10 @@ var h24 = time.Hour * 24
 // c formats a timestamp as HH:MM
 func c(t time.Time) string {
 	return fmt.Sprintf("%.2d:%.2d", t.Hour(), t.Minute())
+}
+
+// exists checks if the given path exists
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
